01eac8e608a5c96b021dd5c03dc786c4a13a38bd
latter-jacob <latter.jacob@gmail.com>
2016-05-14 08:12:52 -0400

    this is a whole bunch off stuff

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,22 @@
+class HomesController < ApplicationController
+
+  def index
+  end
+
+  def about
+  end
+
+  def contactUs
+  end
+
+  def resources
+  end
+
+  def services
+  end
+
+  def incentives
+  end
+
+
+end
