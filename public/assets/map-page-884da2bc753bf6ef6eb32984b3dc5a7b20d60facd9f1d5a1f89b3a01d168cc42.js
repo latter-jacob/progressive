a50433d2152372a200b89c40d035dcea6a3b0dcd
latter-jacob <latter.jacob@gmail.com>
2016-05-24 14:31:54 -0400

    map is now a page that is full screen in mobile

--- public/assets/map-page-884da2bc753bf6ef6eb32984b3dc5a7b20d60facd9f1d5a1f89b3a01d168cc42.js
+++ public/assets/map-page-884da2bc753bf6ef6eb32984b3dc5a7b20d60facd9f1d5a1f89b3a01d168cc42.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([42.4374, -71.7606], 9);
+var mymap2 = L.map('mapid2').setView([42.4374, -71.7606], 9);
 //
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v8/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFrZTg5NDAiLCJhIjoiY2lucnB2d3R3MTBqeHVrbHk0NGM5ajRjcCJ9.ts4qRmVk4Wf7wyKwomlAKw', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -7,20 +7,10 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v8/tiles/{z}/{x}/{y
     accessToken: 'pk.eyJ1IjoiamFrZTg5NDAiLCJhIjoiY2lucnB5OWl3MTBqenVrbHl6dWxkMWwxNyJ9._UKAJjElysUixu5b7S-xNg',
     tileSize: 512,
     zoomOffset: -1
-}).addTo(mymap);
+}).addTo(mymap2);
 
 var circle = L.circle([42.4374, -71.7606], 40233.6, {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.1
-}).addTo(mymap);
-// var popup = L.popup();
-// function onMapClick(e) {
-//   popup
-//     .setLatLng(e.latlng)
-//     .setContent("You clicked the map at " + e.latlng.toString())
-//     .openOn(mymap);
-// }
-// mymap.on('click', onMapClick);
-
-// circle.bindPopup("I am a circle.");
+}).addTo(mymap2);
